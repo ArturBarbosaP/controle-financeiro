2f5de89fff5ec7f618a22349222a7dc90a47caef
Artur Barbosa <arturbarbosap@gmail.com>
2026-02-18 10:43:13 -0300

    Adicionado Entity Framework e criada a classe de contexto do projeto

---
+++ MoneyWeb/Data/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyWeb.Models.Entities;
+
+namespace MoneyWeb.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {}
+    }
+}
