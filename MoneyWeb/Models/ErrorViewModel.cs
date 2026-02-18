6358a875ad9fac7016965dcacc18e36816ef5acd
Artur Barbosa <arturbarbosap@gmail.com>
2026-02-18 09:44:17 -0300

    Adicionar arquivos de projeto.

---
+++ MoneyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoneyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
