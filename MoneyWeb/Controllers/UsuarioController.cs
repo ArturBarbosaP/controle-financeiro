9654df8329fe7f5443b2fbfe91bfa0416fc4f507
Artur Barbosa <arturbarbosap@gmail.com>
2026-02-18 17:18:23 -0300

    Criado formulário para criar novo usuário e adicionado repositório de usuário na controller

--- MoneyWeb/Controllers/UsuarioController.cs
+++ MoneyWeb/Controllers/UsuarioController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MoneyWeb.Repository.Interfaces;
 
 namespace MoneyWeb.Controllers
 {
     public class UsuarioController : Controller
     {
+        private readonly IUsuarioRepository _repository;
+
+        public UsuarioController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View("UsuarioForm");
         }
     }
 }
