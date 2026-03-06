0e3a06c12f491231175b9d211c70487f4f4d484e
Artur Barbosa <arturbarbosap@gmail.com>
2026-03-06 19:41:03 -0300

    Criado BaseController com método para exibir mensagens de sucesso ou erro na tela

---
+++ MoneyWeb/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyWeb.Controllers
+{
+    public class BaseController : Controller
+    {
+        public IActionResult ExibirMensagem(string mensagem, bool sucesso, string action, string controller = null)
+        {
+            TempData[sucesso ? "Sucesso" : "Erro"] = mensagem;
+
+            if (controller != null && controller != string.Empty)
+                return RedirectToAction(action, controller);
+
+            return RedirectToAction(action);
+        }
+    }
+}
