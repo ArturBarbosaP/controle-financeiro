5a9f1fdeb327ccacad45b76b1804239d587acbec
Artur Barbosa <arturbarbosap@gmail.com>
2026-03-06 20:58:28 -0300

    Ajustado formulário de cadastro do usuário para exibir melhor campos com erros de validação

---
+++ MoneyWeb/Helpers/HtmlHelpers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MoneyWeb.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static string GetValidationCssClass(this IHtmlHelper htmlHelper, string propriedade)
+        {
+            var modelState = htmlHelper.ViewData.ModelState;
+
+            if (modelState.ContainsKey(propriedade) && modelState[propriedade].Errors.Any())
+                return "is-invalid";
+
+            return string.Empty;
+        }
+    }
+}
