c32bc1c3e114227a991282f8105607dda224e6e4
Artur Barbosa <arturbarbosap@gmail.com>
2026-03-06 19:42:51 -0300

    Herdado BaseController na UsuarioController, adicionado método para criar usuário no banco e exibir mensagens de erro ou sucesso na tela

--- MoneyWeb/Controllers/UsuarioController.cs
+++ MoneyWeb/Controllers/UsuarioController.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using MoneyWeb.Models.Entities;
+using MoneyWeb.Models.ViewModels;
 using MoneyWeb.Repository.Interfaces;
+using System.Threading.Tasks;
 
 namespace MoneyWeb.Controllers
 {
-    public class UsuarioController : Controller
+    public class UsuarioController : BaseController
     {
         private readonly IUsuarioRepository _repository;
         private readonly IMapper _mapper;
@@ -23,6 +26,29 @@ namespace MoneyWeb.Controllers
         public IActionResult Create()
         {
             return View("UsuarioForm");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UsuarioViewModel usuario)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View("UsuarioForm", usuario);
+
+                Usuario usuarioInsert = _mapper.Map<Usuario>(usuario);
+                _repository.Add(usuarioInsert);
+
+                if (!await _repository.SaveChanges())
+                    return ExibirMensagem("Não foi possível salvar o usuário no banco de dados!", false, "Index", "Usuario");
+
+                return ExibirMensagem("Usuário criado com sucesso!", true, "Index", "Usuario");
+            }
+            catch (Exception ex)
+            {
+                return ExibirMensagem($"Erro ao criar usuário: {ex.Message}", false, "Index", "Usuario");
+            }
         }
     }
 }
