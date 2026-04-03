473a02009e2bd7a41e9170c13209d461da2848dc
Artur Barbosa <arturbarbosap@gmail.com>
2026-04-03 14:38:46 -0300

    Criado repository de Limite

---
+++ MoneyWeb/Repository/Interfaces/ILimiteRepository.cs
@@ -0,0 +1,11 @@
+﻿using MoneyWeb.Models.Entities;
+
+namespace MoneyWeb.Repository.Interfaces
+{
+    public interface ILimiteRepository : IBaseRepository
+    {
+        Task<IEnumerable<Limite>> GetLimites(int usuarioId);
+
+        Task<Limite> GetLimiteById(int id, int usuarioId);
+    }
+}
